2ef47c596f416d3e4d1e5a2e8436ae5583136ee0
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2025-11-04 23:47:44 +0530

    Create Program17_3.java

---
+++ Assignments/Assignment 17/Program17_3.java
@@ -0,0 +1,20 @@
+class Logic
+{
+    void findMax(int a, int b)
+    {
+        if(a > b)
+            System.out.println(a + " is maximum");
+        else
+            System.out.println(b + " is maximum");
+    }
+}
+
+class Program17_3
+{
+    public static void main(String args[])
+    {
+        Logic obj = new Logic();
+        obj.findMax(20,15);
+    }
+}
+
