3f0a5e6af974cc303284378428da38f0be7239f4
Ashwiniyele2316 <ashwiniyele9@gmail.com>
2025-11-05 00:05:05 +0530

    Create Program18_3.java

---
+++ Assignments/Assignment 18/Program18_3.java
@@ -0,0 +1,23 @@
+class Logic
+{
+    void printOddNumbers(int n)
+    {
+        for(int i = 1; i <= n; i++)
+        {
+            if(i % 2 != 0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+class Program18_3
+{
+    public static void main(String args[])
+    {
+        Logic obj = new Logic();
+        obj.printOddNumbers(20);
+    }
+}
+
